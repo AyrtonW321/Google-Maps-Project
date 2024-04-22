8d1c1a31fbd8cec0a8fee41e74b29037fb03e907
Ayrton Wong <[ayrtonwongg@gmail.com]>
2024-04-22 13:05:12 -0400

    Added timers (no conditionals yet)

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 'use strict'
 
-const timerDisplay = document.getElementById('timer');
-const minutes = document.getElementById('minutes');
+let timerRunning = false;
 
-const timerInterval = setInterval(updateTimer, 1000);
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
 
-function startTimer(){
-    timeLeft--;
-    timerDisplay.textContent = ${timeLeft};
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
-    }
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
 }
 
+function pomodoro(){
+  let twentyFiveMinutes = 60 * 1;
+  timerRunning = true;
+  const displayTime = document.getElementById('timer');
+  startTimer(twentyFiveMinutes, displayTime);
+}
+
+function shortBreak(){
+  let fiveMinutes = 60 * 5;
+  const displayTime = document.getElementById('timer');
+  startTimer(fiveMinutes, displayTime);
+}
+
+function longBreak(){
+  let fifteenMinutes = 60 * 15;
+  const displayTime = document.getElementById('timer');
+  startTimer(fifteenMinutes, displayTime);
+}

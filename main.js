e778d1257b7b2d68b9d2155199888caac6fd7574
Ayrton Wong <[ayrtonwongg@gmail.com]>
2024-04-24 02:03:47 -0400

    Add timer functionality

--- main.js
+++ main.js
@@ -1,39 +1,86 @@
-'use strict'
+const countdown = document.getElementById("countdown");
+const startStopButton = document.getElementById("startStop");
+const resetButton = document.getElementById("reset");
+const pomodoroButton = document.getElementById("pomodoro");
+const shortBreakButton = document.getElementById("shortBreak");
+const longBreakButton = document.getElementById("longBreak");
 
-let timerRunning = false;
+let startingMinutes = 25;
+let time = startingMinutes * 60;
+let timerActive = true;
 
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+function displayTime() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  countdown.innerHTML = `${minutes < 10 ? "0" + minutes : minutes} : ${seconds < 10 ? "0" + seconds : seconds}`;
+}
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+function checkTime() {
+  return time >= 0;
+}
 
-      display.textContent = minutes + ":" + seconds;
+function activateTimer() {
+  timerActive = true;
+}
 
-      if (--timer < 0) {
-          timer = duration;
+function updateTime() {
+  displayTime();
+  time--;
+}
+
+function startCountdown() {
+  setTimeout(function () {
+    if (timerActive) {
+      if (checkTime()) {
+        updateTime();
+        startCountdown();
       }
+    }
   }, 1000);
 }
 
-function pomodoro(){
-  let twentyFiveMinutes = 60 * 1;
-  timerRunning = true;
-  const displayTime = document.getElementById('timer');
-  startTimer(twentyFiveMinutes, displayTime);
+function changeCountdownAmount(mins, text) {
+  startingMinutes = mins;
+  time = startingMinutes * 60;
+  timerActive = false;
+  displayTime();
 }
 
-function shortBreak(){
-  let fiveMinutes = 60 * 5;
-  const displayTime = document.getElementById('timer');
-  startTimer(fiveMinutes, displayTime);
+function clickResetButton() {
+  startStopButton.innerHTML = "Start";
+  time = startingMinutes * 60;
+  timerActive = false;
+  displayTime();
 }
 
-function longBreak(){
-  let fifteenMinutes = 60 * 15;
-  const displayTime = document.getElementById('timer');
-  startTimer(fifteenMinutes, displayTime);
+function clickStartStopButton() {
+  if (startStopButton.innerHTML === "Start") {
+    startStopButton.innerHTML = "Stop";
+    activateTimer();
+    startCountdown();
+  } else {
+    startStopButton.innerHTML = "Start";
+    timerActive = false;
+  }
 }
+
+function clickPomodoroButton() {
+  changeCountdownAmount(25, "Pomodoro");
+  startStopButton.innerHTML = "Start";
+}
+
+function clickShortBreakButton() {
+  changeCountdownAmount(5, "Short Break");
+  startStopButton.innerHTML = "Start";
+}
+
+function clickLongBreakButton() {
+  changeCountdownAmount(10, "Long Break");
+  startStopButton.innerHTML = "Start";
+}
+
+startStopButton.addEventListener("click", clickStartStopButton);
+resetButton.addEventListener("click", clickResetButton);
+pomodoroButton.addEventListener("click", clickPomodoroButton);
+shortBreakButton.addEventListener("click", clickShortBreakButton);
+longBreakButton.addEventListener("click", clickLongBreakButton);
